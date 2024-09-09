ba839714e8e983a8b4d9b40c9206480228ad0b05
linxiaoxin <lin_xiaoxin@hotmail.com>
2024-09-09 22:27:44 +0800

    Added healthchecks and updated route

--- question/server.py
+++ question/server.py
@@ -1,12 +1,15 @@
 
 from fastapi import FastAPI
 from langserve import add_routes
-
+from fastapi_healthcheck import HealthCheckFactory, healthCheckRoute
 from question.chain import gen_by_topic_gemini, gen_by_topic_openai
+
+_healthChecks = HealthCheckFactory()
 app = FastAPI(title="Quemistry GenAI")
 
-add_routes(app, gen_by_topic_gemini(), path="/geminimcqbytopic")
-add_routes(app, gen_by_topic_openai(), path="/openaimcqbytopic")
+add_routes(app, gen_by_topic_gemini(), path="/genai-ms/geminimcqbytopic")
+add_routes(app, gen_by_topic_openai(), path="/genai-ms/openaimcqbytopic")
+app.add_api_route('/genai-ms/health', healthCheckRoute(factory=_healthChecks))
 
 if __name__ == "__main__":
     import uvicorn
